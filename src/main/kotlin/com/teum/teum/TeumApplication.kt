596b1ae32bc62930428ffb1c63d6b64ed11f9815
이종범 <jb12333@naver.com>
2026-05-25 18:06:56 +0900

    Spring Boot Kotlin 초기 세팅 완료

---
+++ src/main/kotlin/com/teum/teum/TeumApplication.kt
@@ -0,0 +1,11 @@
+package com.teum.teum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeumApplication
+
+fun main(args: Array<String>) {
+	runApplication<TeumApplication>(*args)
+}
---
+++ src/test/kotlin/com/teum/teum/TeumApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teum.teum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
